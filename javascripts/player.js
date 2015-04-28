9225c9ea52d7b6787e7d1cbc962ac33d1988cb33
akosua kernizan <akernizan@gmail.com>
2015-04-28 17:07:05 -0400

    WIP made playing working on movement

---
+++ javascripts/player.js
@@ -0,0 +1,27 @@
+function Player(context){
+
+}
+
+Player.prototype.moveRight = function(){
+	playerX += 5;
+}
+
+Player.prototype.moveLeft = function(){
+	playerX -= 5;
+}
+
+Player.prototype.moveUp = function(){
+	playerY -= 5;
+}
+
+Player.prototype.moveDown = function(){
+	playerY += 5;
+}
+
+Player.prototype.render = function(){
+		context.strokeStyle ='#2dbd3a ';
+    context.fillStyle = '#2dbd3a ';
+    context.lineWidth = 3;
+    context.fillRect( 0, 0, 20, 20);
+    context.stroke();
+}
