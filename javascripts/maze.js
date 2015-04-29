84e1d4c96eefe77a89b4ccf7bdcddcaabe8466f1
akosua kernizan <akernizan@gmail.com>
2015-04-29 08:19:23 -0400

    added final walls

--- javascripts/maze.js
+++ javascripts/maze.js
@@ -14,36 +14,54 @@ Maze.prototype.render = function(){
 	context.fillRect(20,535,990,25);
 	context.fillRect(985, 20, 25, 450);
 
-	context.fillRect(300,10,25,250);//walls
+	//walls
+	context.fillRect(300,10,25,250);
 	context.fillRect(300,140, 300, 25);
-	// context.fillRect(550,140,25,140);
-	
-
 	context.fillRect(200,150, 100, 25);
+	
+	
+	// walls coming from top border
+	context.fillRect(400,10,25,85);
+	context.fillRect(580,10,25,85);
+
 
 	//inner walls starting from right
-	context.fillRect(820, 400, 25, 140);
-	context.fillRect(695, 400, 125, 25);
-	context.fillRect(695,200, 25, 200);
+	context.fillRect(850, 150, 25, 140);
+	context.fillRect(730, 150, 175, 25);
+	context.fillRect(800, 265, 50,25)
+	context.fillRect(730,10, 25, 150);
+
+	// right walls
+	context.fillRect(900,75,100,25);
+	context.fillRect(800,400,200,25);
+
+
+	//middle walls
+	context.fillRect(475,400,25,140);
+	context.fillRect(500,400,200,25);
+	context.fillRect(550,300,25,100);
+	context.fillRect(575,300,100,25);
+	context.fillRect(650,200,25,100);
 	
-//left wall 
+	//left wall 
 	context.fillRect(10,100, 140,25);
 	context.fillRect(10,200, 120,25);
-	context.fillRect(10,350, 200,25);
-
-//inner walls starting from left
+	
+	//inner walls starting from left
 	context.fillRect(100, 350, 25,108);
 	context.fillRect(100, 455, 300, 25);
 	context.fillRect(300,355, 25, 100);
 	context.fillRect(300, 350, 100,25);
 	context.fillRect(375, 250, 25, 100);
 	context.fillRect(375,250,150,25);
-	context.fillRect(465, 250, 25,120);
-	context.fillRect(465,350, 120, 25);
-
-
+	context.fillRect(500, 150, 25,120);
 
 	context.fillRect(175, 250, 150, 25);
+	
+
+
+
+	
 
 	context.stroke();
 }
