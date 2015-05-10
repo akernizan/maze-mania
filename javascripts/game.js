1387a0a747f347c34d332ab5fc03f98c89829cdc
akosua kernizan <akernizan@gmail.com>
2015-05-09 22:31:38 -0400

    added score and text wrap functions

--- javascripts/game.js
+++ javascripts/game.js
@@ -1,6 +1,6 @@
 function Game() {
-	this.sizeCanvas();
 	this.setCanvas();
+	this.sizeCanvas();
 	this.keyListen();
 	// this.player();
 
@@ -12,6 +12,8 @@ function Game() {
 
 	this.badge = new Badges(this.context);
 	this.badge.render();
+
+	 this.score = 0;
 }
 
 Game.prototype.sizeCanvas = function() {
@@ -35,17 +37,86 @@ Game.prototype.keyListen = function() {
 	$(window).keydown(function(event){
 		var code = event.keyCode;
 		var thisPlayer = that.player;
-		
+
 		if(code == 37){
-			playerX -= 10;
+			// playerX -= 10;
+			thisPlayer.moveLeft();
 			
 		}else if(code == 39){
-			playerX += 10;
+			// playerX += 10;
+			thisPlayer.moveRight();
+
 			console.log(playerX);
 		}else if(code == 38){
-			playerY -= 10; 
+			// playerY -= 10; 
+			thisPlayer.moveUp();
 		}else if(code == 40){
-			playerY += 10; 
+			// playerY += 10; 
+			thisPlayer.moveDown();
 		}
+		that.render();
 	});
+}
+
+Game.prototype.render = function() {
+	this.context.clearRect(0, 0, this.width, this.height);
+	this.maze.render();
+	this.player.render();
+	this.badge.render();
+}
+
+Game.prototype.setGameInterval = function(){
+	var game = this;
+	var player = this.player;
+
+	this.runInterval = setInterval(function(){
+		player.render();
+	},30)
+}
+
+Game.prototype.collide = function() {
+
+}
+
+ Game.prototype.wrapText = function(context, text, x, y, maxWidth, lineHeight) {
+  var words = text.split(' ');
+  var line = '';
+
+  for(var n = 0; n < words.length; n++) {
+    var testLine = line + words[n] + ' ';
+    var metrics = context.measureText(testLine);
+    var testWidth = metrics.width;
+    if (testWidth > maxWidth && n > 0) {
+      context.fillText(line, x, y);
+      line = words[n] + ' ';
+      y += lineHeight;
+    }
+    else {
+      line = testLine;
+    }
+  }
+  context.fillText(line, x, y);
+}
+
+ Game.prototype.gameDirections = function(){
+	context.fillStyle = '#333';
+	context.font = 'bold 16px Arial';
+	context.fillText('Directions' , 1015,80);
+
+  var maxWidth = 200;
+  var lineHeight = 22;
+  var x = 1015;
+  var y = 100;
+  var text = 'Collect all of the badges by moving the player using the up, down, left, and right arrow keys.';
+
+  context.font = '12px Arial';
+  context.fillStyle = '#333';
+
+  wrapText(context, text, x, y, maxWidth, lineHeight);
+}
+
+Game.prototype.gameScore = function() {
+	context.fillStyle = '#333';
+	context.font = 'bold 26px Arial';
+	context.fillText('SCORE : ' + score, 1015,40);
 }
